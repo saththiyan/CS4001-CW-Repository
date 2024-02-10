68341c518f0c7c94536d883b39034912c356db0f
Saththiyan Parameswaran <shavav@gmail.com>
2024-02-10 02:04:17 +0000

    text

---
+++ Mp3.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class Mp3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Mp3
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Mp3
+     */
+    public Mp3()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    
+    
+    public void print(){
+    
+        
+        System.out.println("Hi");
+        System.out.println("Hello");
+        System.out.println("Hi");
+        System.out.println("Hello");
+    
+    }
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
